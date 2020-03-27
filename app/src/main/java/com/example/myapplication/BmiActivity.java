6b3a0b8c49e0f51c2a13c8d8e5666cbddc7ea75e
lukaskolehotsky <lukaskolehotsky@gmail.com>
2020-03-27 09:44:37 +0100

    age spinner

--- app/src/main/java/com/example/myapplication/BmiActivity.java
+++ app/src/main/java/com/example/myapplication/BmiActivity.java
@@ -1,12 +1,16 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,15 +24,26 @@ import com.example.myapplication.utilities.FileReader;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class BmiActivity extends AppCompatActivity {
+public class BmiActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private static DecimalFormat df2 = new DecimalFormat("#.##");
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String AGE = "age";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+
+        Spinner ageSpinner = (Spinner) findViewById(R.id.ageSpinner);
+        ArrayAdapter<CharSequence> ageAdapter = ArrayAdapter.createFromResource(this, R.array.ages, android.R.layout.simple_spinner_item);
+        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ageSpinner.setAdapter(ageAdapter);
+
+        ageSpinner.setOnItemSelectedListener(this);
 
         Button resultButton = (Button) findViewById(R.id.resultButton);
         resultButton.setOnClickListener(new View.OnClickListener() {
@@ -36,15 +51,13 @@ public class BmiActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText weightEditText = (EditText) findViewById(R.id.weightEditText);
                 EditText heightEditText = (EditText) findViewById(R.id.heightEditText);
-                EditText ageEditText = (EditText) findViewById(R.id.ageEditText);
                 TextView bmiTextView = (TextView) findViewById(R.id.bmiTextView);
                 TextView categoryTextView = (TextView) findViewById(R.id.categoryTextView);
 
                 double weight = Double.parseDouble(weightEditText.getText().toString());
                 double height = Double.parseDouble(heightEditText.getText().toString());
-                int age = Integer.parseInt(ageEditText.getText().toString());
+                int age = Integer.parseInt(loadData(AGE).toString());
 
-//                ArrayList<AgeWithBmis> ageWithBmisList = new FileReader(getBaseContext()).processFile(R.raw.locations);
                 ArrayList<JsonProperty> jsonProperty = new FileReader(getBaseContext()).processFile(R.raw.locations);
 
                 for(Vitamin vitamin: jsonProperty.get(0).getWomanVitamins()){
@@ -96,6 +109,33 @@ public class BmiActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        saveData(AGE, text);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private void saveData(String name, String value){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(name, value);
+
+        editor.apply();
+    }
+
+    private String loadData(String name){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String value = sharedPreferences.getString(name, "no value");
+
+        return value;
     }
 
 }
